7cadd9b658b1290f6f5876dbe0164b79426ad8e3
Pravin Mhaske <pravinmhaske817@gmail.com>
2023-10-08 18:59:14 +0530

    Authentication Using Clerk added successfully.

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function DashboardPage() {
-  return <p>DashBoard (Protected)</p>;
+  return (
+    <div>
+      <p>DashBoard (Protected)</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
